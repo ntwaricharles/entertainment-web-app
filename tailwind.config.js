19129b4124d5b3881aa4ca8fb0faef0436745fad
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-26 11:00:53 +0200

    entertainment web-app

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+   content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FC4747',
+        dark: '#10141E',
+        secondary: '#5A698F',
+        accent: '#161D2F',
+        white: '#FFFFFF'
+      },
+    },
+  },
+  plugins: [],
+};
