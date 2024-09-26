dda859c8a4c474adfaf74e6b67d29cc9e4d84c6f
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-26 10:53:56 +0200

    app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
+import { TVSeriesComponent } from './pages/tv-series/tv-series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tv-series', component: TVSeriesComponent },
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: '**', redirectTo: '' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { movieReducer } from './store/movie.reducer';
+import { MovieEffects } from './store/movie.effects';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TVSeriesComponent } from './pages/tv-series/tv-series.component';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidebarComponent, HomeComponent, MoviesComponent, TVSeriesComponent, BookmarksComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ movies: movieReducer }),
+    EffectsModule.forRoot([MovieEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
