e25a0ea3b9b57b1621436d78ccb391fa7e759075
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-27 12:24:15 +0200

    shared layouts

---
+++ src/app/shared-layout/shared-layout/shared-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-layout',
+  templateUrl: './shared-layout.component.html',
+  styleUrl: './shared-layout.component.css'
+})
+export class SharedLayoutComponent {
+
+}
