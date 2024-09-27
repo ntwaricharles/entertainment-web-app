d7327514f694c744c03d9ddad61dd4760e138b2f
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-27 12:26:07 +0200

    auth environment

---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+ firebaseConfig: {
+  apiKey: "AIzaSyAb6ydanB1uA1uMTqMtNpEiF11KtotMnIk",
+  authDomain: "entertainment-web-app-99902.firebaseapp.com",
+  projectId: "entertainment-web-app-99902",
+  storageBucket: "entertainment-web-app-99902.appspot.com",
+  messagingSenderId: "261849868162",
+  appId: "1:261849868162:web:6795d1d6c4b22d3706919b",
+  measurementId: "G-P6CWSFHDMZ"
+}
+};
