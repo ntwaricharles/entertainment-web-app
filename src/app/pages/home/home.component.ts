ea5414f7bf330bd14120c73824b48fdf14302b2c
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-27 12:21:17 +0200

    entertainment web app pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
+// src/app/pages/home/home.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { loadMovies } from '../../store/movie.actions';
 import {
   selectAllMovies,
   selectMoviesLoading,
 } from '../../store/movie.selector';
+import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +16,33 @@ import {
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  movies$ = this.store.select(selectAllMovies);
+  movies$: Observable<Movie[]> = this.store.select(selectAllMovies);
+  trendingMovies$: Observable<Movie[]> = of([]); 
+  recommendedMovies$: Observable<Movie[]> = of([]); 
   loading$ = this.store.select(selectMoviesLoading);
+  searchQuery: string = ''; 
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadMovies());
+    this.initializeMovieSections();
+  }
+
+  initializeMovieSections(): void {
+    this.trendingMovies$ = this.movies$.pipe(
+      map((movies) => movies.filter((movie) => movie.isTrending))
+    );
+
+    this.recommendedMovies$ = this.movies$.pipe(
+      map((movies) => movies.filter((movie) => !movie.isTrending))
+    );
+  }
+
+  getFilteredMovies(movies: Movie[] | null): Movie[] {
+    if (!movies) return []; 
+    return movies.filter((movie) =>
+      movie.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
   }
 }
